85cd0fc6359f3cde4be2a5dea26b8be664dc8f1b
megha532 <meghapiet532@poornima.org>
2016-07-09 03:18:30 +0530

    Create login.blade.php

---
+++ login.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<!--location :resources>views>login.blade.php -->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>sign in</title>
+    </head>
+    <body>
+        <a href="gauth">Login using Google</a>
+    </body>
+</html>

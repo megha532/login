7de719edc1bdc67806573cfefdfdcee487d852ff
megha532 <meghapiet532@poornima.org>
2016-07-09 03:12:58 +0530

    Create AuthController.php

---
+++ AuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+    public function getGoogleLogin($auth=NULL){
+        if($auth=='auth'){
+            try
+            {
+                Hybrid_Endpoint::process();
+            }
+            catch(Exception $e)
+            {
+                return Redirect::to('gauth');
+            }
+            return;
+        }
+        $oauth = new Hybrid_auth(app_path().'/config/google_auth.php');
+        $provider=$oauth->authentication('Google');
+        $profile=$provider->getUserProfile();
+        return var_dump($profile).'<a href="logout">Log Out</a>';
+    }
+    public function getLoggedOut(){
+        $gauth=new Hybrid_auth(app_path().'/config/google_auth.php');
+        $gauth->logoutAllProviders();
+        return View::make('login');
+    }
+}

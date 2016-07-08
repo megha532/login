bf669e2ee38d588d49f9119399d121b2eeb156ba
megha532 <meghapiet532@poornima.org>
2016-07-09 03:14:11 +0530

    Create google_auth.php

---
+++ google_auth.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "base_url"=>"https://laravel-intro-imemy.c9users.io/gauth/auth",
+    "providers"=>array(
+    "Google"=>array(
+    "enabled"=>true,
+    "keys"=>array("id"=>"576592296733-ju7r7ekepgdjm54k6akbvsqi8bv0ai14.apps.googleusercontent.com","secret"=>"dRz1iL2JOwWB1UlG0biLbEtI"),
+    "scope"=>""."https://www.googleapis.com/auth/userinfo.email"
+    )));
+?>
